11bc5efbfec0ed4ae1c013560ba46fe94332a0a3
Marianita100 <jessicasalgado652@gmail.com>
2026-03-09 17:13:58 -0600

    Delete Integradora2/js directory
    Eliminando carpeta equivocada

--- Integradora2/js/Inventario.js
+++
@@ -1,6 +0,0 @@
-function mostrar(id){
-    let grupos = document.querySelectorAll(".grupo");
-    grupos.forEach(g => g.style.display = "none");
-
-    document.getElementById(id).style.display = "grid";
-}
